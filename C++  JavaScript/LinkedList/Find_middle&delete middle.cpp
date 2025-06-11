42fc11a6767a19eff771a8e7b692381cbd7e7ba5
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2025-06-11 22:16:11 +0530

    update

---
+++ C++  JavaScript/LinkedList/Find_middle&delete middle.cpp
@@ -0,0 +1,11 @@
+/* Here we find  the middle of a Linked List and delete it;
+
+example:
+input: 1 -> 2 -> 3 -> 4 -> 5
+output: 1 -> 2 -> 4 -> 5
+ time complexity: O(n)
+ space complexity: O(1)
+*/
+// c++ code
+
+soluc
